bd01d54ef092d03ec5a81cef04eaa25588fe78e2
Your Name <you@example.com>
2024-11-23 13:44:23 +0400

    first commit

---
+++ UniqloMVC/Program.cs
@@ -0,0 +1,28 @@
+namespace UniqloMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.MapControllerRoute(name:"default", 
+                pattern: "{controller =home}/{action=index}/{id?}");
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            
+            app.Run();
+        }
+    }
+}
